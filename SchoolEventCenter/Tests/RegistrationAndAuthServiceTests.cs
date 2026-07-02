3fa4c6fe22dba5926d6e24d9d5960f383946be52
LRRashkov22 <LRRashkov22@codingburgas.bg>
2026-07-02 17:30:23 +0300

    Add QA architecture

---
+++ SchoolEventCenter/Tests/RegistrationAndAuthServiceTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Moq;
+using SchoolEventCenter.Module.Data.Domain.Entities;
+using SchoolEventCenter.Module.Data.Domain.Events;
+using SchoolEventCenter.Module.Data.Domain.Enums;
+using SchoolEventCenter.Module.Data.Options;
+using SchoolEventCenter.Module.Data.Persistance;
+using SchoolEventCenter.Module.Data.Shared;
+using SchoolEventCenter.Module.Data.Shared.Common;
+using SchoolEventCenter.Module.Registrations.Application.Service;
+using ScoolEventCenter.Module.Identity.Application.DTOs;
+using ScoolEventCenter.Module.Identity.Application.Services;
+using Xunit;
+
+namespace Tests;
+
+public class RegistrationAndAuthServiceTests : IDisposable
+{
+   
+}
---
+++ SchoolEventCenter/Tests/SchoolEventCrudAndQueryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolEventCenter.Module.Data.Domain.Entities;
+using SchoolEventCenter.Module.Data.Domain.Events;
+using SchoolEventCenter.Module.Data.Domain.Enums;
+using SchoolEventCenter.Module.Data.Persistance;
+using SchoolEventCenter.Module.Data.Shared;
+using SchoolEventCenter.Module.Data.Shared.Common;
+using SchoolEvemtCenter.Module.SchoolEventsManagement.Application.DTOs.OrganizerDTOs;
+using SchoolEvemtCenter.Module.SchoolEventsManagement.Application.Service;
+using Xunit;
+
+namespace Tests;
+
+public class SchoolEventCrudAndQueryTests
+{
+   
+}
